c91db1de74115d12546cac1be2ad72907c67855a
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2017-06-16 14:01:54 +0200

    Adding the checkpoint

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ from keras.callbacks import *
 from builder import *
 from data import *
 
-VECTOR_SIZE=10
-MEMORY_SIZE=3
-SEQ_LENGTH=7
+VECTOR_SIZE=50
+MEMORY_SIZE=150
+SEQ_LENGTH=50
 INCLUDE_PROB=0.5
 
-NB_TRAIN=5000
+NB_TRAIN=1000
 NB_TESTS=100
+
+BATCH_SIZE=100
+NB_EPOCH=15
 
 SAVE_DIR="models/"
 
@@ -23,7 +26,7 @@ if __name__ == "__main__":
     if(len(sys.argv) > 2 and sys.argv[1] == "load"):
         print("Loading model...")
         model = load_model(SAVE_DIR+sys.argv[2], 
-                {'IO_Layer': IO_Layer})
+                {'IO_Heads': IO_Heads})
         save_name = sys.argv[2]
     else:
         print("Building new model...")
@@ -45,14 +48,17 @@ if __name__ == "__main__":
     x_in, y_in = include_batch(NB_TRAIN, 
             SEQ_LENGTH, 
             VECTOR_SIZE)
-
-    print("x: ", x_in.shape)
-    print("y: ", y_in.shape)
-
+    
     print("Saving model...")
     model.save(SAVE_DIR+save_name)
+    
+    checkpoint = ModelCheckpoint(SAVE_DIR+save_name)
+
     print("Training...")
-    model.fit(x_in, y_in) 
+    model.fit(x_in, y_in,
+            batch_size=BATCH_SIZE,
+            epochs=NB_EPOCH,
+            callbacks=[checkpoint]) 
 
     print("Saving model...")
     model.save(SAVE_DIR+save_name)
@@ -63,10 +69,18 @@ if __name__ == "__main__":
             VECTOR_SIZE)  
     
     pred = model.predict(x_in)
+    pred = [[p > 0.5 for p in seq] for seq in pred]
+    solu = [[p > 0.5 for p in seq] for seq in y_in]
+    total = 0
 
-    pred = [p > 0.5 for p in pred]
-    res = [p > 0.5 for p in y_in]
-    ok = sum([1 for x, y in zip(pred, res) if x == y  ])
-    print("Nb tests:   ", NB_TESTS)
-    print("Nb success: ", ok)
+    # print("Pred: ", pred)
+    # print("Solu: ", solu)
+
+    for i, p in enumerate(pred):
+        ok = sum([1 for x, y in zip(p, solu[i]) if x == y  ])
+        total += ok
+        # print("Sequence ", i+1, ": ", ok, " / ", SEQ_LENGTH )
+    print("Nb sequences: ", NB_TESTS)
+    print("Nb tests in each sequence: ", SEQ_LENGTH)
+    print("Nb success: ", total, " / ", NB_TESTS*SEQ_LENGTH )
 
