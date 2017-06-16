d18ed3dc62e0cec03e7b7d2c92d641a18ed91d19
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2017-06-16 19:38:12 +0200

    Adding LSTM to compare

--- layers/builder.py
+++ layers/builder.py
@@ -2,11 +2,17 @@ from io_heads import *
 from keras.layers import *
 from keras.models import *
 
-def build_RNN(input_shape, memory_size, vect_size, output_size):
+def build_RNN(input_shape, memory_size, vect_size, output_size, depth):
     model = Sequential()
     model.add(IO_Heads(memory_size=memory_size, 
         vector_size=vect_size, 
         input_shape=input_shape,
-        return_sequences=True))
-    model.add(Dense(1, activation='sigmoid'))
+        return_sequences=True,
+        depth=depth))
+    model.add(Dense(output_size))
+    return model
+
+def build_LSTM(input_shape, vect_size, output_size):
+    model = Sequential()
+    model.add(LSTM(output_size, input_shape=input_shape, return_sequences=True))
     return model
