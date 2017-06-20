2677ec176e480357b0636b13d73ae401417b880d
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2017-06-20 21:57:26 +0200

    New inmpel, to return mem

--- layers/builder.py
+++ layers/builder.py
@@ -1,19 +1,25 @@
 from io_heads import *
+from io_generator import *
 from keras.layers import *
 from keras.models import *
 
 def build_RNN(input_shape, memory_size, vect_size, output_size, depth):
     model = Sequential()
+    model.add(IO_Generator(output_size=memory_size+1+vect_size,
+        memory_size=memory_size,
+        input_shape=input_shape,
+        name="GENERATION"))
     model.add(IO_Heads(memory_size=memory_size, 
         vector_size=vect_size, 
-        input_shape=input_shape,
+        output_size=vect_size*2,
         return_sequences=True,
-        depth=depth))
-    model.add(Dense(output_size))
+        name="MAIN"))
+    model.add(Dense(output_size, activation="sigmoid"))
+
     return model
 
 def build_LSTM(input_shape, vect_size, output_size):
     model = Sequential()
     model.add(LSTM(output_size, input_shape=input_shape, return_sequences=True))
-    model.add(Dense(output_size, activation='softmax'))
+    model.add(Dense(output_size, activation='sigmoid'))
     return model
--- layers/io_heads.py
+++ layers/io_heads.py
@@ -3,124 +3,93 @@ import tensorflow as tf
 
 from keras import backend as K
 from keras.engine.topology import Layer
-from keras.layers import *
+from keras.layers import Activation, Recurrent
+from keras.initializers import *
 
 
 class IO_Heads(Recurrent):
-    def __init__(self, memory_size, vector_size, depth, **kwargs):
+    def __init__(self, memory_size, vector_size, output_size, **kwargs):
         print("Initialisating IO_Heads...")
         self.memory_size = memory_size
-        self.units = vector_size
-        self.vect_size = vector_size
-        self.depth = depth
+        self.units = output_size + memory_size*vector_size
+        self.vector_size = vector_size
         super(IO_Heads, self).__init__(**kwargs)
 
     def build(self, input_shape):
         print("Building IO_Heads...")
         self.memory = self.add_weight(
-                shape=(self.memory_size, self.vect_size),
-                initializer="uniform",
+                shape=(self.memory_size, self.vector_size),
+                initializer="zeros",
                 trainable=False,
                 name="MEMORY")
-        
-        self.initiator = self.add_weight(
-                shape=(self.vect_size*2, 
-                    self.vect_size*3+self.memory_size),
-                initializer='uniform',
-                trainable=True,
-                name="Initiator")
-
-        self.generators = []
-        for i in range(self.depth):
-            self.generators.append(self.add_weight(
-                shape=(self.vect_size*3+self.memory_size, 
-                    self.vect_size*3+self.memory_size),
-                initializer='uniform',
-                trainable=True,
-                name="Generator-"+str(i)))
-
-        self.post_treatment = self.add_weight(
-               shape=(self.vect_size*2, self.units),
-               initializer='uniform',
-               trainable=True,
-               name="PostTreatment")
-
+        print("Input_shape: ", input_shape) 
         self.memory.eval(K.get_session())
-        self.states = [None]
+        self.states=[None]
+        self.seq_size=input_shape[1]
         self.built = True
 
     def step(self, inputs, states):
         def dist(x, y):
-           diff = tf.norm(tf.subtract(x, y))
-           return diff
+            diff = tf.norm(tf.subtract(x, y))
+            return diff
 
-            #  p = tf.matmul(x, tf.reshape(y, shape=(self.vect_size,1)))
-            #  nx = tf.norm(x)
-            #  ny = tf.norm(y)
-            #
-            #  d = tf.divide(p, tf.multiply(nx, ny))
-            #  d = tf.reshape(d, ())
-            #  l = tf.constant(1000.)
-            #  return tf.minimum(d, l)
-            #
         def focus_by_content(x):
-            x = tf.reshape(x, (1, self.vect_size))
-            dists = tf.map_fn(lambda y: dist(x, y), self.memory)
-            tot = tf.reduce_sum(dists, axis=0)
-            tot = tf.maximum(tot, tf.constant(0.001))
-            return tf.divide(dists, tot)
+            dists = tf.map_fn(
+                    lambda y: dist(x, tf.reshape(y, (self.vector_size, ))),
+                    self.memory)
+            dists = tf.nn.softmax(dists)
+            return tf.nn.softmax(dists)
 
         def read_mem(weight):
-            weight = tf.reshape(weight, (1, self.memory_size))
+            weight = tf.nn.softmax(tf.reshape(weight, (1, self.memory_size)))
+            print("weight: ", weight)
             r = tf.matmul(weight, self.memory)
+            r = tf.nn.softmax(r)
+            print("Read: ", r)
+            #  r = tf.reshape(r, (self.vector_size,))
             return r
 
         def write_mem(weight, erase, vector):
+            weight = tf.nn.softmax(weight)
             weight = tf.reshape(weight, (self.memory_size, 1))
-            erase  = tf.reshape(erase,  (1, self.vect_size))
-            eraser = tf.matmul(weight, erase)
-            self.memory = tf.subtract(self.memory,
-                    tf.multiply(self.memory, eraser))
-            adder = tf.multiply(weight, vector)
+            vector = tf.reshape(vector, (1, self.vector_size))
+            adder = K.dot(weight, vector)
             self.memory = tf.add(self.memory, adder)
 
         print("Calling IO_Head...")
-        if self.states[0] is None:
-            print("Reseting memory")
-            self.memory = tf.random_uniform((self.memory_size, self.vect_size), 0, 1,  dtype="float32")
-            self.states[0] = tf.constant(0., shape=[1, self.vect_size], dtype="float32")
+        inputs = inputs[0]
+        print("Inputs: ", inputs.shape)
+        x, w_weight, erase = tf.split(inputs, 
+            [self.vector_size, self.memory_size, 1], axis=0)
 
-        vs = self.vect_size
-        ms = self.memory_size
-        inputs=tf.reshape(inputs[0], (1, self.vect_size))
-        stat = tf.reshape(states[0][0], (1, self.vect_size))
-        
-        entry = tf.concat([inputs, stat], axis=1)
-        
-        start = tf.matmul(entry, self.initiator)
-
-        for i in range(self.depth):
-            start = tf.matmul(start, self.generators[i])
-
-        r_vect, w_weight, w_vect, erase = tf.split(start, 
-                [vs, ms, vs, vs], axis=1)
-        r_weight = focus_by_content(r_vect)
-
-        print("Writing...")
-        write_mem(w_weight, erase, w_vect)
+        r_weight = focus_by_content(x)
 
         print("Reading...")
         read = read_mem(r_weight)
-        post = tf.concat([inputs, read], axis=1)
-        ret = tf.matmul(post, self.post_treatment)
+
+        print("Writing...")
+            #  w_weight = tf.nn.sigmoid(w_weight)
+        write_mem(w_weight, erase, x)   
+       
+        flat = tf.reshape(self.memory, (1, self.memory_size*self.vector_size))
+        ret = tf.concat([read, flat], axis = -1)
+        print("Ret: ", ret)
         return ret, [ret]
 
     def get_config(self):
         config = {
                 'memory_size': self.memory_size, 
-                'vector_size': self.vect_size,
-                'depth'      : self.depth
+                'vector_size': self.vector_size,
+                'output_size': self.units
         }
         base_config = super(IO_Heads, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
+    #  def compute_output_shape(self, input_shape):
+    #      output_shape = (input_shape[0], input_shape[1], self.units)
+    #      return output_shape
+#  class ResetMemory(Callback):
+#      def on_batch_begin(self, batch, logs={}):
+#          self.layer[0] = tf.random_uniform((self.memory_size, self.vect_size), 0, 1,  dtype="float32")
+#
+    
