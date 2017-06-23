c9e0ac26137b9945f4409634152b977fa794e9b5
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2017-06-23 14:23:28 +0200

    preprocessing now inside the layer for recursivity

--- layers/io_heads.py
+++ layers/io_heads.py
@@ -8,15 +8,21 @@ from keras.initializers import *
 
 
 class IO_Heads(Recurrent):
-    def __init__(self, memory_size, entry_size, **kwargs):
+    def __init__(self, units, memory_size, entry_size, **kwargs):
         print("Initialisating IO_Heads...")
         self.memory_size = memory_size
-        self.units = entry_size
+        self.units = units
         self.entry_size = entry_size
         super(IO_Heads, self).__init__(**kwargs)
 
     def build(self, input_shape):
         print("Building IO_Heads...")
+        self.pre_treat = self.add_weight(
+                shape=(self.units, self.memory_size+3*self.entry_size),
+                initializer="uniform",
+                trainable=True,
+                name="PRE")
+        
         self.memory = self.add_weight(
                 shape=(self.memory_size, self.entry_size),
                 initializer=RandomUniform(minval=0., maxval=0.25),
@@ -24,13 +30,20 @@ class IO_Heads(Recurrent):
                 name="MEMORY")
        
         self.memory.eval(K.get_session())
-        self.state_shape = (self.memory_size, self.entry_size)
-        self.states=[tf.random_uniform(shape=self.state_shape, minval=0., maxval=0.25, dtype="float32")]
-        self.seq_size=input_shape[1]
+
+        self.post = self.add_weight(
+                shape=(self.units*2, self.units),
+                initializer="uniform",
+                trainable=True,
+                name="POST")
+
+        self.mem_shape = (self.memory_size, self.entry_size)
+        self.states=[tf.constant(0., shape=(1, self.units)), tf.random_uniform(shape=self.mem_shape, minval=0., maxval=0.25, dtype="float32")]
         self.built = True
 
     def get_initial_state(self, inputs):
-        return [tf.random_uniform(shape=self.state_shape, minval=0., maxval=0.25, dtype="float32")]
+        print("inputs, ", inputs)
+        return [tf.reshape(inputs[0][0],(1, self.units)), tf.random_uniform(shape=self.mem_shape, minval=0., maxval=0.25, dtype="float32")]
 
     def step(self, inputs, states):
         def dist(x, y):
@@ -59,28 +72,35 @@ class IO_Heads(Recurrent):
 
         # _, m = tf.split(states[0], [self.vector_size, self.memory_size*(self.vector_size+2)], axis=1)
         print("Getting previous memory state...")
-        m = tf.reshape(states[0], (self.memory_size, self.entry_size))
-        self.memory = m
+        self.memory = states[1]
+        print("in: ", states[0])
         
-        inputs = inputs[0]
-        r_vect, w_vect, w_weight, erase = tf.split(inputs, 
-            [self.entry_size, self.entry_size, self.memory_size, self.entry_size], axis=0)
+        inputs = states[0]
+        
+        pre = K.dot(tf.reshape(inputs, (1, self.units)), self.pre_treat)
 
-        print("Reading...")
+        print("pre: ", pre)
+
+        r_vect, w_vect, w_weight, erase = tf.split(pre, 
+            [self.entry_size, self.entry_size, self.memory_size, self.entry_size], axis=1)
+
+        print("Reading... ", r_vect)
         r_weight = focus_by_content(r_vect)
         read = read_mem(r_weight)
         
         print("Writing...")
         write_mem(w_weight, w_vect, erase)   
-      
-        print("Read: ", read)
-        return read, [self.memory]
+        
+        r = K.concatenate([read, tf.reshape(inputs, (1, self.units))], axis=1)
+        res = K.dot(r, self.post)
+        print("Ret: ", res)
+        return res, [res, self.memory]
 
     def get_config(self):
         config = {
                 'memory_size': self.memory_size, 
                 'entry_size': self.entry_size,
-                'output_size': self.units
+                'units': self.units
         }
         base_config = super(IO_Heads, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
