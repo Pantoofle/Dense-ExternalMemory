6a67dd648363d29f1d718be788d79aa232bdf189
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2017-06-28 17:19:28 +0200

    Adapting to the automaton question

--- main.py
+++ main.py
@@ -9,47 +9,55 @@ from keras.layers import *
 from io_heads import *
 from data import *
 
-VECTOR_SIZE=10
-SEQ_LEN=10
-MEMORY_SIZE=8
+# Params used to generate data 
+STATES=5
+ALPHABET=5
+LENGTH=7
+
+# Network params
+MEMORY_SIZE=20
 ENTRY_SIZE=5
-
 DEPTH=3
-READ_HEADS=4
+READ_HEADS=3
 
+# Training params
 NB_TRAIN=2000
 NB_TESTS=100
-
 BATCH_SIZE=1
-NB_EPOCH=100
+NB_EPOCH=300
 
+# Dir where models will be saved
 SAVE_DIR="models/"
 
 if __name__ == "__main__":
     print("Starting process...")
     print("Getting data...")
-    #  scalar = float(input("Spare time multiplicator: "))
-    #  wait, l, x_in, y_in = syracuse_batch(NB_TRAIN, VECTOR_SIZE, scalar)
-    x_in, y_in = include_batch(NB_TRAIN, SEQ_LEN, VECTOR_SIZE)
+    x_in, y_in, tot = automaton_batch(NB_TRAIN, STATES, ALPHABET, LENGTH)
+    #  x_in, y_in = include_batch(NB_TRAIN, LENGTH, ALPHABET)
 
+    for i in range(10):
+        print("Test ", i, " -> ", x_in[i], "\n  Res: ", y_in[i])
+    
+    #  print("Nb_succes, ", tot, " over ", NB_TRAIN)
     print("Building the first layer...")
-    inputs = Input(shape=(SEQ_LEN, VECTOR_SIZE))
+    inputs = Input(shape=(LENGTH, ALPHABET))
 
     path = input("Path where to load/save the model: ")
     if path == "":
         path = "full"
 
     memory = IO_Heads(units=1,
-            vector_size=VECTOR_SIZE,
+            vector_size=ALPHABET,
             memory_size=MEMORY_SIZE, 
             entry_size=ENTRY_SIZE, 
             name="MAIN",
+            #  return_sequences=True,
             read_heads=READ_HEADS,
-            depth=DEPTH,
-            return_sequences=True)(inputs)
+            depth=DEPTH)(inputs)
     
     model = Model(inputs=inputs, outputs=memory)
 
+    # Load if asked
     if len(sys.argv) > 1 and sys.argv[1]=="load":
         print("Loading the full layer...")
         model = load_model(SAVE_DIR+path+".h5",
@@ -65,8 +73,10 @@ if __name__ == "__main__":
     model.summary()
     print("IN: ", x_in.shape)
     print("OUT: ", y_in.shape)
+
+
     if not(len(sys.argv) > 1 and sys.argv[1] == "notrain"):
-        print("Training second layer...")
+        print("Training ...")
         model.fit(x_in, y_in,
                 batch_size=BATCH_SIZE,
                 epochs=NB_EPOCH,
@@ -75,37 +85,11 @@ if __name__ == "__main__":
                     #  EarlyStopping(monitor='val_acc', min_delta=0.005, patience = 5),
                     TensorBoard(log_dir='./logs', histogram_freq=1, 
                         write_graph=True, 
-                        write_images=True)])
+                        write_images=False)])
 
-        print("Saving the full model...")
+        print("Saving the model...")
         save_model(model, SAVE_DIR+path+".h5")
     
     print("Theoretical: ", 
             VECTOR_SIZE*1./SEQ_LEN * (1.-(1.-1./VECTOR_SIZE)**SEQ_LEN) )
 
-    #  import matplotlib.pyplot as plt
-    #  plt.figure(1)
-    #  plt.subplot(245)
-    #  plt.imshow(concaten[-1][-4].reshape(1, 2*VECTOR_SIZE))
-    #  plt.subplot(246)
-    #  plt.imshow(concaten[-1][-3].reshape(1, 2*VECTOR_SIZE))
-    #  plt.subplot(247)
-    #  plt.imshow(concaten[-1][-2].reshape(1, 2*VECTOR_SIZE))
-    #  plt.subplot(248)
-    #  plt.imshow(concaten[-1][-1].reshape(1, 2*VECTOR_SIZE))
-    #
-    #  plt.subplot(241)
-    #  plt.imshow(mem_img[-1][-4].reshape((MEMORY_SIZE, VECTOR_SIZE+2)))
-    #  plt.subplot(242)
-    #  plt.imshow(mem_img[-1][-3].reshape((MEMORY_SIZE, VECTOR_SIZE+2)))
-    #  plt.subplot(243)
-    #  plt.imshow(mem_img[-1][-2].reshape((MEMORY_SIZE, VECTOR_SIZE+2)))
-    #  plt.subplot(244)
-    #  plt.imshow(mem_img[-1][-1].reshape((MEMORY_SIZE, VECTOR_SIZE+2)))
-    #
-    #  plt.savefig("img/memory.png")
-
-    #  plt.clf()
-    #  plt.figure(1)
-    #  plt.imshow(inter_pred[-1])
-    #  plt.savefig("img/inter.png")
