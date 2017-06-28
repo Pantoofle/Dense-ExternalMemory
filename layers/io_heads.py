953a12e690c1c2f063ae8c17b86e26af1bb70f6e
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2017-06-28 17:19:03 +0200

    Adding moar recursivity

--- layers/io_heads.py
+++ layers/io_heads.py
@@ -9,6 +9,17 @@ from keras.initializers import *
 
 class IO_Heads(Recurrent):
     def __init__(self, units, vector_size, memory_size, entry_size, depth=0, read_heads=1, **kwargs):
+        """ 
+        Initialise the network
+            - units       : output size 
+            - vector_size : size of one element from the input sequence
+            - memory_size : number of memory slots
+            - entry_size  : size of a memory slot 
+            - depth       : nb of Dense layer applied to generate the memory parameters 
+                            and after read to return the output
+            - read_heads  : number of read heads 
+        """
+        
         print("Initialisating IO_Heads...")
         self.memory_size = memory_size
         self.vector_size = vector_size
@@ -19,13 +30,22 @@ class IO_Heads(Recurrent):
         super(IO_Heads, self).__init__(**kwargs)
 
     def build(self, input_shape):
+        """
+        Builds the network, sets the initial weights
+        """
+        
+        
         print("Building IO_Heads...")
+
+        # Generates the memory access params 
         self.pre_treat = self.add_weight(
-                shape=(self.vector_size, self.memory_size+(2+self.read_heads)*self.entry_size),
+                shape=(self.vector_size + self.entry_size, 
+                    self.memory_size+(2+self.read_heads)*self.entry_size),
                 initializer="uniform",
                 trainable=True,
                 name="PRE")
         
+        # The deap network
         self.deep_pre = []
         for i in range(self.depth):
             self.deep_pre.append( self.add_weight(
@@ -34,54 +54,75 @@ class IO_Heads(Recurrent):
                     trainable = True,
                     name = "DEEP_PRE_"+str(i)))
 
+        # The memory, note the 'trainable=False'
         self.memory = self.add_weight(
                 shape=(self.memory_size, self.entry_size),
                 initializer=RandomUniform(minval=0., maxval=0.25),
                 trainable=False,
                 name="MEMORY")
        
+        # Because it is not trainable we need to evaluate it before 
         self.memory.eval(K.get_session())
 
+        # Deep 
         self.deep_post = []
         for i in range(self.depth):
             self.deep_post.append( self.add_weight(
-                    shape = (self.vector_size + self.read_heads*self.entry_size,self.vector_size + self.read_heads*self.entry_size),
+                    shape = (self.vector_size + self.entry_size + self.read_heads*self.entry_size, 
+                        self.vector_size + self.entry_size + self.read_heads*self.entry_size),
                     initializer = "uniform",
                     trainable = True,
                     name = "DEEP_POST_"+str(i)))
 
-
+        # Generates the output from the read vectors 
         self.post = self.add_weight(
-                shape=(self.vector_size + self.read_heads*self.entry_size, self.units),
+                shape=(self.vector_size + self.entry_size + self.read_heads*self.entry_size, 
+                    self.units),
                 initializer="uniform",
                 trainable=True,
                 name="POST")
 
+        # The memory and prev state will be given to the next state 
         self.mem_shape = (self.memory_size, self.entry_size)
-        self.states=[tf.random_uniform(shape=self.mem_shape, minval=0., maxval=0.25, dtype="float32")]
+        self.states=[tf.constant(0., shape=(self.entry_size,)),
+                tf.random_uniform(shape=self.mem_shape, minval=0., maxval=0.25, dtype="float32")]
         self.built = True
 
     def get_initial_state(self, inputs):
-        print("inputs, ", inputs)
-        return [tf.random_uniform(shape=self.mem_shape, minval=0., maxval=0.25, dtype="float32")]
+        """
+        Initialisation of the states = initial memory content
+        """
+        return [tf.constant(0., shape=(self.entry_size,)), self.memory]
 
     def step(self, inputs, states):
         def dist(x, y):
+            """
+            Distance between two vectors. Was cosine in the begining but too messy with a random initial memory
+            """
             m = tf.matmul(tf.reshape(x, (1, self.entry_size)) , y)
             return m
 
         def focus_by_content(x):
+            """
+            Generates the weight vector
+            """
             dists = tf.map_fn(
                     lambda y: dist(x, tf.reshape(y, (self.entry_size, 1))),
                     self.memory)
             return dists
 
         def read_mem(weight):
+            """
+            Applies the weight and produces the read vector
+            """
             weight = tf.reshape(weight, (1, self.memory_size))
             r = tf.matmul(weight, self.memory)
             return r
 
         def write_mem(weight, vector, eraser):
+            """
+            Writes vector to memory with weight
+            """
             weight = tf.reshape(weight, (self.memory_size, 1))
             vector = tf.reshape(vector, (1, self.entry_size))
             eraser = tf.reshape(eraser, (1, self.entry_size))
@@ -90,21 +131,23 @@ class IO_Heads(Recurrent):
             self.memory = tf.subtract(self.memory, subb)
             self.memory = tf.add(self.memory, adder)
 
-        # _, m = tf.split(states[0], [self.vector_size, self.memory_size*(self.vector_size+2)], axis=1)
         print("Getting previous memory state...")
-        self.memory = states[0]
+        self.memory = states[1]
         
-        inputs = inputs[0]       
+        inputs = K.concatenate([inputs[0], states[0]], axis=-1)
+
         print("in: ", inputs)
 
-        pre = K.dot(tf.reshape(inputs, (1, self.vector_size)), self.pre_treat)
+        # Applies the first layers
+        pre = K.dot(tf.reshape(inputs, (1, self.vector_size + self.entry_size)), self.pre_treat)
         
+        # Deep enough 
         for i in range(self.depth):
             pre = K.dot(pre, self.deep_pre[i])
 
         print("pre: ", pre)
         
-        
+        # Extracts data from the generated vector
         w_vect, w_weight, erase, r_vect = tf.split(pre, 
             [self.entry_size, self.memory_size, self.entry_size, self.entry_size*self.read_heads], axis=1)
 
@@ -114,7 +157,6 @@ class IO_Heads(Recurrent):
             r_vectors.append(r)
 
         print("Reading... ")
-
         reads = []
         for i in range(self.read_heads):
             r_weight = focus_by_content(r_vectors[i])
@@ -124,14 +166,15 @@ class IO_Heads(Recurrent):
         print("Writing...")
         write_mem(w_weight, w_vect, erase)   
         
-        r = K.concatenate(reads+[tf.reshape(inputs, (1, self.vector_size))], axis=1)
+        # The post operations
+        r = K.concatenate(reads+[tf.reshape(inputs, (1, self.vector_size + self.entry_size))], axis=1)
 
         for i in range(self.depth):
             r = K.dot(r, self.deep_post[i])
 
-        res = K.dot(r, self.post)
+        res = tf.minimum( K.dot(r, self.post), tf.constant(1., shape=(1, 1)))
         print("Ret: ", res)
-        return res, [self.memory]
+        return res, [tf.reshape(reads[0], (self.entry_size, )), self.memory]
 
     def get_config(self):
         config = {
@@ -144,11 +187,3 @@ class IO_Heads(Recurrent):
         base_config = super(IO_Heads, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
-    #  def compute_output_shape(self, input_shape):
-    #      output_shape = (input_shape[0], input_shape[1], self.units)
-    #      return output_shape
-#  class ResetMemory(Callback):
-#      def on_batch_begin(self, batch, logs={}):
-#          self.layer[0] = tf.random_uniform((self.memory_size, self.vect_size), 0, 1,  dtype="float32")
-#
-    
