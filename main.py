5e974a1dcd377c762b7eeb478bbc3d769757a35a
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2017-07-11 18:31:33 +0200

    Adding comparaison possibilities between networks

--- main.py
+++ main.py
@@ -15,23 +15,21 @@ from generate_graph import *
 
 # Train params
 ALPHABET=2
-MIN_LENGTH=2
+MIN_LENGTH=3
 MAX_LENGTH=6
 
 # Network params
-MEMORY_SIZE=4
-ENTRY_SIZE=4
+MEMORY_SIZE=15
+ENTRY_SIZE=10
 DEPTH=1
-READ_HEADS=1
+READ_HEADS=2
 
 # Training params
-TRAIN_PER_SIZE=256
 BATCH_SIZE=1
-NB_EPOCH=10
+MEM_EPOCH=30
+LSTM_EPOCH=50
 NB_TRAIN=10
-NB_TESTS=10
-
-THRESHOLD=0.5
+NB_WORDS=5000
 
 # Dir where models will be saved
 SAVE_DIR="models/"
@@ -40,13 +38,7 @@ if __name__ == "__main__":
     print("Starting process...")
     print("Getting automaton...")
     automaton = automaton1()
-    #  x_in, y_in = include_batch(NB_TRAIN, LENGTH, ALPHABET)
-
-    #  for i in range(10):
-        #  print("Test ", i, " -> ", x_in[i], "\n  Res: ", y_in[i])
-    
-    #  print("Nb_succes, ", tot, " over ", NB_TRAIN)
-    print("Building the first layer...")
+    print("Building the models...")
     inputs = Input(shape=(None, ALPHABET))
 
     memory = IO_Heads(units=1,
@@ -69,35 +61,65 @@ if __name__ == "__main__":
                   loss='mean_squared_error',
                   metrics=['accuracy'])
     
-    model2.compile(optimizer='adadelta',
+    model2.compile(optimizer='rmsprop',
                   loss='mean_squared_error',
                   metrics=['accuracy'])
-   
-    if not(len(sys.argv) > 1 and sys.argv[1] == "notrain"):
+ 
+    load = input("Load the models? (y/n) ")
+    load = (load == "y")
+    train = True
+
+    if load:
+        model.load_weights(SAVE_DIR+"DNC.h5")
+        model2.load_weights(SAVE_DIR+"LSTM.h5")
+        train = (input("Train again? (y/n) ") == "y")    
+  
+    if train:
         for i in range(NB_TRAIN):        
             length=np.random.randint(MIN_LENGTH, MAX_LENGTH+1)
             if i == 0:
                 length = MIN_LENGTH
             print("Train ", i+1, " of ", NB_TRAIN, ": word length: ", length)
-            x_in, y_in, tot = automaton_batch(TRAIN_PER_SIZE, ALPHABET, length, automaton=automaton)
-            print("Rate: ", tot*1./TRAIN_PER_SIZE, " - ", 1-tot*1./TRAIN_PER_SIZE)
-            print("Training ...")
+            train = 2**(length+1)
+            x_in, y_in, tot = automaton_batch(train, ALPHABET, length, automaton=automaton)
+            print("Rate: ", tot*1./train, " - ", 1-tot*1./train)
+            print("Training DNC ...")
             model.fit(x_in, y_in,
                 batch_size=BATCH_SIZE,
-                epochs=NB_EPOCH)
-            #  model2.fit(x_in, y_in,
-            #      batch_size=BATCH_SIZE,
-            #      epochs=NB_EPOCH)
-    
+                epochs=MEM_EPOCH)
+                #  callbacks=[EarlyStopping(monitor='loss', min_delta=0.005, patience=10)])
+           
+            print("Training LSTM ...")
+            model2.fit(x_in, y_in,
+                batch_size=BATCH_SIZE,
+                epochs=LSTM_EPOCH)
+                #  callbacks=[EarlyStopping(monitor='loss', min_delta=0.005, patience=10)])
+
+    save = input("Shall I save the models ? (y/n): ")
+    save = (save=="y")
+    if save:
+        model.save_weights(SAVE_DIR+"DNC.h5")
+        model2.save_weights(SAVE_DIR+"LSTM.h5")
+
+
     print("Generating infered automaton")
-    res = alf_infere(model, automaton, THRESHOLD, ALPHABET)
-    with open("graph.dot", "w+") as file:
-        file.write(res[:-3])
+   
+    tpr, fpr = test_network(model, automaton, ALPHABET, BATCH_SIZE, NB_WORDS, MIN_LENGTH, MAX_LENGTH)
+
+    tpr2, fpr2 = test_network(model2, automaton, ALPHABET, BATCH_SIZE, NB_WORDS, MIN_LENGTH, MAX_LENGTH)
+    
+    trace_ROC([tpr, tpr2], [fpr, fpr2])
+    
+    keep = (input("Keep dnc? ") == "y")
+    with open("dnc_dots.txt", "w+") as file:
+        
+        for t, f in zip(tpr, fpr):
+            file.write(str(t)+" "+str(f)+"\n")
 
 
-    #  tpr, fpr = test_network(model, automaton, NB_TESTS, ALPHABET, BATCH_SIZE, NB_WORDS, STOP_RATE, MAX_LENGTH)
-    #  trace_ROC(tpr, fpr)
+    keep = (input("Keep lstm? ") == "y")
+    with open("lstm_dots.txt", "w+") as file:
+        for t, f in zip(tpr, fpr):
+            file.write(str(t)+" "+str(f)+"\n")
 
-    #  tpr, fpr = test_network(model2, automaton, NB_TESTS,
-    #          STATES, ALPHABET, BATCH_SIZE, NB_WORDS, STOP_RATE, MAX_LENGTH)
-    #
+
